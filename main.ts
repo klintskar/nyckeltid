15c6b65bdde3ee81a31db581e2b94906cd6eb578
klintskar <113171445+klintskar@users.noreply.github.com>
2022-09-12 11:30:35 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,23 @@
+let keyTimeVar = 0
+let Time = 0
+let keyTime = 0
+input.onGesture(Gesture.ScreenDown, function () {
+    if (keyTimeVar < 10) {
+        keyTimeVar += 1
+    } else {
+        keyTimeVar = 0
+    }
+})
+function timeCalc (num: number) {
+    Time = 3 / 4 * 2 ** num
+    return Time
+}
+basic.forever(function () {
+    basic.showNumber(keyTime)
+})
+basic.forever(function () {
+    if (keyTime > 0) {
+        control.waitMicros(10 ** 6 * 60)
+        keyTime += -1
+    }
+})
